5f92285a14218e0a8fe78519c01caa37d494af2b
AlvinSeville215 <Armageddon884@gmail.com>
2017-07-15 19:31:55 +0300

    Changed method add(); Add method remove() and set();

--- yaroslav/src/main/java/hw05_1/List.java
+++ yaroslav/src/main/java/hw05_1/List.java
@@ -1,9 +1,11 @@
 package hw05_1;
 
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
-public class List <T> {
-// CONSTANTS
+public class List<T> {
+    // CONSTANTS
     final private int DEFAULT_CAPACITY = 10;
     private int size = DEFAULT_CAPACITY;
     private T[] array;
@@ -16,63 +18,95 @@ public class List <T> {
 
     public List(int size) {
         this.array = (T[]) new Object[size];
+        this.cash = (T[]) new Object[size];
     }
 
-    public List(){
+    public List() {
         this.array = (T[]) new Object[size];
+        this.cash = (T[]) new Object[size];
     }
 
     private void rangeCheck(int index) {
         if (index >= size)
             throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
     }
+
     private String outOfBoundsMsg(int index) {
-        return "Index: "+index+", Size: "+size;
+        return "Index: " + index + ", Size: " + size;
     }
 
-    private void checkCapasity(){
+    private void checkCapasity() {
         int calc = 0;
-        for(int index = 0; index < size; index++){
-            if(array[index] == null){
+        for (int index = 0; index < size; index++) {
+            if (array[index] == null) {
                 calc++;
             }
         }
-        if(calc == 0){
+        if (calc > size * 0.7) {
             size = size / 2 * 3 + 1;
         }
     }
 
-    public void add(T elem){
+    public void add(T elem) {
         checkCapasity();
-        for (int index = 0; index < size; index++){
-            if(array[index] == null){
+        for (int index = 0; index < size; index++) {
+            if (array[index] == null) {
                 array[index] = elem;
                 break;
             }
         }
     }
 
-    public void add(int index, T elem){
+    public void add(int index, T elem) {
         checkCapasity();
         rangeCheck(index);
-        array[index] = elem;
+        cash = Arrays.copyOf(array, index - 1);
+        cash[index] = elem;
+        for (int i = index + 1; i < size; i++) {
+            cash[i] = array[i - 1];
+        }
+        array = Arrays.copyOf(cash, size);
     }
 
-    public int size(){
+    public void remove(int index){
+        cash = Arrays.copyOf(array, index - 1);
+        for (int i = index; i < size; i++) {
+            cash[i] = array[index];
+        }
+    }
+
+    public void remove(T elem){
+        for (int index = 0; index < size; index++) {
+            if (array[index] == elem){
+                cash = Arrays.copyOf(array, index - 1);
+                for (int i = index; i < size; i++) {
+                    cash[i] = array[index];
+                }
+                break;
+            }
+        }
+    }
+
+    public int size() {
         return size;
     }
 
-    public T get(int index){
+    public T get(int index) {
         return array[index];
     }
 
-    public int indexOf(T elem){
+    public int indexOf(T elem) {
         for (int i = 0; i < size; i++) {
-            if (elem == array[i]){
+            if (elem == array[i]) {
                 return i;
             }
         }
         return -1;
+    }
+
+    public void set(int index, T elem) {
+        rangeCheck(index);
+        array[index] = elem;
     }
 
 }
